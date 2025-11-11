6a38a817f2352386fe1dcfa2e462df185091c42e
TomislavTkalecDev <tomislav.tkalec99@gmail.com>
2025-11-11 21:48:21 +0100

    add comment

--- dateTime.php
+++ dateTime.php
@@ -21,21 +21,21 @@ if ($newYear->isSunday() || $newYear->isSaturday()) {
 
 exit;
 // DateTime Example 
-$christmasDate = new DateTime('2025-12-25 12:00:00');
-$nowDate = new DateTime();
+// $christmasDate = new DateTime('2025-12-25 12:00:00');
+// $nowDate = new DateTime();
 
-$day = $christmasDate->format('l');
+// $day = $christmasDate->format('l');
 
-$diff = $christmasDate->diff($nowDate);
+// $diff = $christmasDate->diff($nowDate);
 
-$christmasDate->setTimezone(new DateTimeZone('America/New_York'));
-echo $christmasDate->format('Y-m-d H:i:s T'); // 2025-11-11 08:45:00 EST
+// $christmasDate->setTimezone(new DateTimeZone('America/New_York'));
+// echo $christmasDate->format('Y-m-d H:i:s T'); // 2025-11-11 08:45:00 EST
 
-if ($day == 'Sunday' || $day == 'Saturday') {
-    echo $diff->days . ' to ' . $christmasDate->format('d.m.Y') . ' ,Day is weekend.';
-    exit;
-}else{
-    echo $diff->days . ' to ' .  $christmasDate->format('d.m.Y') . ' ,Day is not weekend.';
-    exit;
-}
+// if ($day == 'Sunday' || $day == 'Saturday') {
+//     echo $diff->days . ' to ' . $christmasDate->format('d.m.Y') . ' ,Day is weekend.';
+//     exit;
+// }else{
+//     echo $diff->days . ' to ' .  $christmasDate->format('d.m.Y') . ' ,Day is not weekend.';
+//     exit;
+// }
 
